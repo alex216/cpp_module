b4f88a555bf7a67219e0960a5df5875f69b4b6b1
yliu <yliu@XLIJFELKNIX.local>
2025-06-04 15:54:52 +0900

    [fix] update main

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -5,11 +5,13 @@
 
 int main()
 {
+	std::cerr << "------------------" << std::endl;
 	{
 		Ice ice;
 		Cure cure;
 	}
 
+	std::cerr << "------------------" << std::endl;
 	{
 		IMateriaSource *src = new MateriaSource();
 		(void)src;
@@ -27,6 +29,35 @@ int main()
 		delete bob;
 		delete me;
 		delete src;
+	}
+
+	std::cerr << "------------------" << std::endl;
+	{
+		Character ch("alice");
+		Ice *m1 = new Ice();
+		Ice *m2 = new Ice();
+		Cure *m3 = new Cure();
+		Cure *m4 = new Cure();
+		Ice *m5 = new Ice();
+
+		ch.equip(m1);
+		ch.equip(m2);
+		ch.equip(m3);
+		ch.equip(m4);
+		// fails
+		ch.equip(m5);
+		delete m5;
+
+		ch.unequip(1);
+		delete m2;
+
+		ICharacter *me = new Character("me");
+		ch.use(1, *me);
+		ch.use(2, *me);
+		ch.use(3, *me);
+		ch.use(4, *me);
+
+		delete me;
 	}
 
 	return 0;
